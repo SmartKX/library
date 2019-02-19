c8fabd83d12adcb41f8d5c4ec45c801d3aa2ba0a
Cody Marx Bailey <superphly@gmail.com>
2019-02-19 12:38:53 -0600

    encryption round trip test

--- test.js
+++ test.js
@@ -7,3 +7,4 @@ skx.getBalance().then(r => console.log('Balances\n' + JSON.stringify(r)));
 skx.proxy.getContracts(0).then(r => console.log('Contract Addresses (' + r.length + ')\n' + JSON.stringify(r)));
 
 skx.encrypt('foobar').then(r => console.log('Encrypt\n' + JSON.stringify(r)));
+skx.encrypt('foobar').then(r => skx.decrypt(r)).then(r => console.log(r));
